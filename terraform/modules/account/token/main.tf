b40712acab7b8b3bad53aef8ce1f753e54bdebfb
Ryan Rousseau <r@ryanrousseau.com>
2024-08-22 16:40:26 -0500

    Add token account

--- terraform/modules/account/token/main.tf
+++ terraform/modules/account/token/main.tf
@@ -6,14 +6,12 @@ terraform {
   }
 }
 
-resource "octopusdeploy_ssh_key_account" "account" {
+resource "octopusdeploy_token_account" "account" {
   name                              = var.name
-  private_key_file                  = var.private_key_file
-  username                          = var.username
+  token                             = var.token
   space_id                          = var.space_id
   description                       = var.description
   environments                      = var.environments
-  private_key_passphrase            = var.private_key_passphrase
   tenant_tags                       = var.tenant_tags
   tenanted_deployment_participation = var.tenanted_deployment_participation
   tenants                           = var.tenants
