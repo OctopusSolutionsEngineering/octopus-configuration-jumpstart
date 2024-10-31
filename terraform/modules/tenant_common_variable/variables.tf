59b9bb88b44137b49418ce1084bb1dead977718b
Ryan Rousseau <r@ryanrousseau.com>
2024-10-31 09:55:41 -0500

    Add tenant_common_variable resource

---
+++ terraform/modules/tenant_common_variable/variables.tf
@@ -0,0 +1,22 @@
+variable "library_variable_set_id" {
+  type = string
+}
+
+variable "space_id" {
+  type    = string
+  default = null
+}
+
+variable "template_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "value" {
+  type      = string
+  sensitive = true
+  default   = null
+}
