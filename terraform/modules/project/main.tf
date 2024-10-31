c6ebadfae5843f23253e4cba35bcb60bda290460
Ryan Rousseau <r@ryanrousseau.com>
2024-10-31 09:42:43 -0500

    Add initial draft of project resource

---
+++ terraform/modules/project/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    octopusdeploy = {
+      source = "OctopusDeployLabs/octopusdeploy"
+    }
+  }
+}
+
+resource "octopusdeploy_project" "project" {
+  name         = var.name
+  lifecycle_id = var.lifecycle_id
+  project_group_id = var.project_group_id
+  space_id     = var.space_id
+
+  auto_create_release = var.auto_create_release
+
+  # connectivity policy
+
+  default_guided_failure_mode = var.default_guided_failure_mode
+  default_to_skip_if_already_installed = var.default_to_skip_if_already_installed
+  deployment_changes_template = var.deployment_changes_template
+  description = var.description
+
+  # git anonymous persistent settings
+  # git library credential persistent settings
+  # git username password persistent settings
+
+  included_library_variable_sets = var.included_library_variable_sets
+  is_disabled = var.is_disabled
+  is_discrete_channel_release = var.is_discreet_channel_release
+
+  # is version controlled
+
+  jira_service_management_extension_settings {
+    is_enabled = var.jira_service_management_extension_settings_is_enabled
+    connection_id = var.jira_service_management_extension_settings_connection_id
+    service_desk_project_name = var.jira_service_management_extension_settings_service_desk_project_name
+  }
+
+  release_notes_template = var.release_notes_template
+  slug = var.slug
+  tenanted_deployment_participation = var.tenanted_deployment_participation
+
+}
---
+++ terraform/modules/project/variables.tf
@@ -0,0 +1,121 @@
+variable "auto_create_release" {
+  type = bool
+  default = false
+}
+
+variable "cac_git_base_path" {
+  type = string
+  default = null
+}
+
+variable "cac_git_credential_id" {
+  type = string
+  default = null
+}
+
+variable "cac_git_default_branch" {
+  type = string
+  default = null
+}
+
+variable "cac_git_password" {
+  type = string
+  default = null
+  sensitive = true
+}
+
+variable "cac_git_protected_branches" {
+  type = set(string)
+  default = null
+}
+
+variable "cac_git_url" {
+  type = string
+  default = null
+}
+
+variable "cac_git_username" {
+  type = string
+  default = null
+}
+
+variable "default_guided_failure_mode" {
+  type = string
+  default = "EnvironmentDefault"
+}
+
+variable "default_to_skip_if_already_installed" {
+  type = bool
+  default = false
+}
+
+variable "deployment_changes_template" {
+  type = string
+  default = ""
+}
+
+variable "description" {
+  type = string
+  default = ""
+}
+
+variable "included_library_variable_sets" {
+  type = list(string)
+  default = null
+}
+
+variable "is_cac_using_git_anonymous" {
+  type = bool
+  default = false
+}
+
+variable "is_cac_using_git_library_credential" {
+  type = bool
+  default = false
+}
+
+variable "is_cac_using_git_username_password" {
+  type = bool
+  default = false
+}
+
+variable "is_disabled" {
+  type = bool
+  default = false
+}
+
+variable "is_discreet_channel_release" {
+  type = bool
+  default = false
+}
+
+variable "is_version_controlled" {
+  type = bool
+  default = false
+}
+
+variable "lifecycle_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "project_group_id" {
+  type = string
+}
+
+variable "release_notes_template" {
+  type = string
+  default = null
+}
+
+variable "space_id" {
+  type = string
+}
+
+variable "tenanted_deployment_participation" {
+  type = string
+  default = "Untenanted"
+}
